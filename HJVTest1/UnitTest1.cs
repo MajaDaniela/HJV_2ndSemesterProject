2fcaa85195b62f2d1acc56b045e9ffcff71abb38
Lukas Lanz <lula54119@edu.ucl.dk>
2023-11-27 10:11:37 +0100

    GetDescription and unittest

---
+++ HJVTest1/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace HJVTest1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
--- HJV_2ndSemesterProject/Models/Rank.cs
+++ HJV_2ndSemesterProject/Models/Rank.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace HJV_2ndSemesterProject.Models
 {
-    enum Rank
+     public enum Rank
     {
+        [Description("Menig")]
+        Private,
+        [Description("Korporal")]
+        Corporal,
+        [Description("Sergent")]
+        Sergeant
     }
 }
