389b5d8b7761d639097582d1fcaa2c0288f73065
JonasL <46361162+JonasLarsen0@users.noreply.github.com>
2023-11-27 09:27:21 +0100

    UI LogInPage update. Without admin and profile page.

--- HJV_2ndSemesterProject/Views/LogInPage.xaml.cs
+++ HJV_2ndSemesterProject/Views/LogInPage.xaml.cs
@@ -18,11 +18,117 @@ namespace HJV_2ndSemesterProject.Views
     /// <summary>
     /// Interaction logic for LogInPage.xaml
     /// </summary>
+    /// 
+
+    public class TestAdmin
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class TestUser
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+
     public partial class LogInPage : UserControl
     {
+        private TestAdmin testAdmin;
+        private TestUser testUser;
         public LogInPage()
         {
             InitializeComponent();
+            InitializeTestUser();
+            InitializeTestAdmin();
+        }
+
+        private void InitializeTestAdmin()
+        {
+            testAdmin = new TestAdmin
+            {
+                Email = "admin@example.com",
+                Password = "adminpassword"
+            };
+        }
+
+        private void InitializeTestUser()
+        {
+            testUser = new TestUser
+            {
+                Email = "test@example.com",
+                Password = "testpassword"
+            };
+        }
+
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            if (textBox.Text == "Indtast email")
+            {
+                textBox.Text = "";
+                textBox.Foreground = System.Windows.Media.Brushes.Black;
+            }
+        }
+
+        private void TextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            if (string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                textBox.Text = "Indtast email";
+                textBox.Foreground = System.Windows.Media.Brushes.Gray;
+            }
+        }
+
+        private void LoginBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //string enteredEmail = emailTextBox.Text;
+            //string enteredPassword = passwordBox.Password;
+
+            //if (IsValidLogin(enteredEmail, enteredPassword, testUser))
+            //{
+            //    // Navigate to the user profile. HAS TO BE CHANGED TO PAGE instead of window
+            //    UserProfileWindow userProfileWindow = new UserProfileWindow();
+            //    userProfileWindow.Show();
+
+
+            //    Window parentWindow = Window.GetWindow(this);
+            //    if (parentWindow != null)
+            //    {
+            //        parentWindow.Close();
+            //    }
+            //}
+            //else if (IsValidLogin(enteredEmail, enteredPassword, testAdmin))
+            //{
+            //    // Navigate to the admin profile  HAS TO BE CHANGED TO PAGE instead of window
+            //    AdminProfileWindow adminProfileWindow = new AdminProfileWindow();
+            //    adminProfileWindow.Show();
+
+            //    Window parentWindow = Window.GetWindow(this);
+            //    if (parentWindow != null)
+            //    {
+            //        parentWindow.Close();
+            //    }
+            //}
+            //else
+            //{
+            //    MessageBox.Show("Forkert email eller adgangskode. Prøv igen..");
+            //}
+        }
+
+        private bool IsValidLogin(string enteredEmail, string enteredPassword, object user)
+        {
+            if (user is TestUser)
+            {
+                return enteredEmail == ((TestUser)user).Email && enteredPassword == ((TestUser)user).Password;
+            }
+            else if (user is TestAdmin)
+            {
+                return enteredEmail == ((TestAdmin)user).Email && enteredPassword == ((TestAdmin)user).Password;
+            }
+
+            return false;
         }
     }
 }
