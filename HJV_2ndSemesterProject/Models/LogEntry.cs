ec15f698669c8ecea992f5ba650147f28f1effb4
Lukas Lanz <lula54119@edu.ucl.dk>
2023-12-06 02:46:15 +0100

    LogEntry full CRUD (not tested)

--- HJV_2ndSemesterProject/Models/LogEntry.cs
+++ HJV_2ndSemesterProject/Models/LogEntry.cs
@@ -8,6 +8,7 @@ namespace HJV_2ndSemesterProject.Models
 {
      public class LogEntry
     {
+        public int Id { get; set; }
         public Role Role {  get; set; }
         public int NumberofMinutes { get; set; }
         public string Comment { get; set; }
@@ -15,13 +16,15 @@ namespace HJV_2ndSemesterProject.Models
         public string MA_Number { get; set; }
         public int SailingID { get; set; }
 
-        public LogEntry(Role role, int minutes, string comment, string ma, int sailingID ) 
+        public LogEntry(Role role, int minutes, string comment, string ma, int sailingID, int id =0 ) 
         {
             Role = role;
             NumberofMinutes = minutes;
             Comment = comment;
             MA_Number = ma;
             SailingID = sailingID;
+            Id = id;
         }
+
     }
 }
